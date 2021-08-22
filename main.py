9f0699e0d4d54f8fcc3cece207e572e1e20b04e6
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-08-22 19:27:08 +0530

    Added state to pass the html example_1.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import os
 from automata import Automata
 
 autoM = Automata()
@@ -15,7 +16,17 @@ autoM.add_transition("T_3", "T_4", "^[\"]$")
 autoM.add_transition("T_4", "T_4", "^[^\"]$")
 autoM.add_transition("T_4", "T_1", "^[\"]$")
 autoM.set_endpoint("T_5")
+autoM.add_transition("T_5", "T_5", "^[\s]$")
+autoM.add_transition("T_5", "T_0", "^[^<\s]$")
+autoM.add_transition("T_5", "T_1", "^[<]$")
 
 print(autoM)
 
 autoM.run('nothing random <tag attribute="attribute_value" attribute_2="attribute_value_2">')
+
+"""Read the example file."""
+EXAMPLE_FILE = os.path.join(os.getcwd(), "html_example", "example_2.html")
+example = open(EXAMPLE_FILE, 'r')
+data = example.read()
+
+autoM.run(data)
