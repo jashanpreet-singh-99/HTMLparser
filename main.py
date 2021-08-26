508f0e67eb43c98f1574d99887db48131277ffbf
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-08-26 23:58:44 +0530

    Restructured the examples and output for better readability.

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from parser import Parser
 
 
 """Read the example file."""
-EXAMPLE_FILE = os.path.join(os.getcwd(), "html_example", "example_2.html")
-example = open(EXAMPLE_FILE, 'r')
+EXAMPLE_FILE_1 = os.path.join(os.getcwd(), "html_example", "example_1.html")
+example = open(EXAMPLE_FILE_1, 'r')
 data = example.read()
 
 html = Parser(data)
-html.save_parsed_html("parsed_html.csv")
+html.save_parsed_html("output/parsed_html_1.csv")
+
+"""Read the example file."""
+EXAMPLE_FILE_2 = os.path.join(os.getcwd(), "html_example", "example_2.html")
+example = open(EXAMPLE_FILE_2, 'r')
+data = example.read()
+
+html_2 = Parser(data)
+html_2.save_parsed_html("output/parsed_html_2.csv")
